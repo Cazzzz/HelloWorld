dca3b8031bd7ca467b0f40320abf2a53d567977f
Cazzzz <yeracscott9@gmail.com>
2020-10-06 10:07:46 +0100

    My first Commit

---
+++ HelloWorld/routes/web.php
@@ -0,0 +1,31 @@
+<?php
+# @Date:   2020-10-06T09:54:32+01:00
+# @Last modified time: 2020-10-06T10:03:12+01:00
+
+
+
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('helloworld', function (){
+  return '<h1>HelloWorld</h1>';
+});
+
+Route::get('scott', function (){
+  return '<h1>ScotCarey</h1>';
+});
